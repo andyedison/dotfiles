d8a4c1b3628645c559bd2af752cdbb00008856f8
Andrew Edison <edison.andrew@gmail.com>
2016-12-09 08:44:04 -0600

    added install script for setting up links for files

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+
+# This will setup the links for the config files. Add to this as you add more files
+
+## NOTE Vim files in separate repo
+
+
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+
